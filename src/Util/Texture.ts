879132e140b9b666fb8f5daeb1e144d3db9d55a1
ben-hunter-hansen <ben.hunter.hansen@gmail.com>
2016-02-28 14:58:14 -0600

    removed Texture.ts

--- src/Util/Texture.ts
+++
@@ -1,6 +0,0 @@
-export class Texture {
-    static load(url: string): Promise<THREE.Texture> {
-        let loader = new THREE.TextureLoader();
-        return Promise.resolve(loader.load(url));
-    }
-}
